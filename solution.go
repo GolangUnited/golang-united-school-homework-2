fb1a579cc3d0f4fa1b7eef3b598f3678c35f3332
Kurmashev-1620 <kasymov.adil@list.ru>
2022-02-19 21:10:19 +0600

    sol

--- solution.go
+++ solution.go
@@ -5,6 +5,7 @@ package square
 // Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
 // it's like:
 // CalcSquare(10.0, SidesTriangle)
+ggreger
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
