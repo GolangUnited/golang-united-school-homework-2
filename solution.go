8884a1dd90dba50b147469bbf855ec4001f9b4ff
Victor <eoll073@gmail.com>
2022-02-20 16:06:09 +0300

    100502 variant

--- solution.go
+++ solution.go
@@ -4,11 +4,13 @@ import "math"
 
 type myType int
 
-func CalcSquare(sideLen float64, sidesNum myType) (res float64) {
+const (
+	SidesCircle   myType = 0
+	SidesTriangle myType = 3
+	SidesSquare   myType = 4
+)
 
-	const SidesCircle myType = 0
-	const SidesTriangle myType = 3
-	const SidesSquare myType = 4
+func CalcSquare(sideLen float64, sidesNum myType) (res float64) {
 
 	if sidesNum == SidesTriangle {
 		res = (math.Pow(sideLen, 2.0) * math.Sqrt(3.0)) / 4
