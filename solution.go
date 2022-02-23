fb32b999a81ad3a315a76f6478d2ef6ef5c92494
Nursultan <nurtuganulyn@gmail.com>
2022-02-23 16:17:19 +0600

    fix

--- solution.go
+++ solution.go
@@ -9,23 +9,23 @@ import "math"
 // CalcSquare(10.0, SidesTriangle)
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
-type myInt int
+type figureSide int
 
 const (
-	SidesCircle   myInt = 0
-	SidesTriangle myInt = 3
-	SidesSquare   myInt = 4
+	SideCircle   figureSide = 0
+	SideTriangle figureSide = 3
+	SideSquare   figureSide = 4
 )
 
-func CalcSquare(sideLen float64, sidesNum myInt) float64 {
+func CalcSquare(sideLen float64, sidesNum figureSide) float64 {
 	var area float64 = 0
 
 	switch sidesNum {
-	case SidesCircle:
+	case SideCircle:
 		area = area + math.Pi*(sideLen*sideLen)
-	case SidesTriangle:
+	case SideTriangle:
 		area = area + (math.Sqrt(3)/4)*(sideLen*sideLen)
-	case SidesSquare:
+	case SideSquare:
 		area = area + sideLen*sideLen
 	}
 	return area
