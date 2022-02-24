1f98b6f01075e04925861c1ea44af70f760e5e32
Ilmiddin Tuychiev <ilmiddin17@mail.ru>
2022-02-24 10:11:47 +0500

    solution by switch case

--- solution.go
+++ solution.go
@@ -10,25 +10,15 @@ import "math"
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
-const (
-	SidesCircle SidesNumber =0
-	SidesTriangle SidesNumber =3
-	SidesSquare SidesNumber =4
-)
-
-func CalcSquare(sideLen float64, sidesNum SidesNumber) float64 {
-
-	switch sidesNum{
-	case SidesTriangle:
-		return (sideLen*sideLen*math.Sqrt(3)/4)
-	
-	case SidesSquare:
-		return (sideLen*sideLen)
-
-	case SidesCircle:
-		return (math.Pi*sideLen*sideLen)
-		
+func CalcSquare(sideLen float64, sidesNum int64) float64 {
+	switch {
+	case sidesNum == 4:
+		return (math.Pow(sideLen, 2))
+	case sidesNum == 3:
+		return (math.Pow(sideLen, 2) * math.Sqrt(3) / 4)
+	case sidesNum == 0:
+		return (math.Pow(sideLen, 2) * math.Pi)
 	default:
 		return (0)
-
+	}
 }
