cf819212ed5e41a518cb5f16706a0ff7942e6fec
Alexey Kabanov <akabanov@vodokanal-nn.ru>
2022-03-01 17:43:31 +0300

    Add constants  On branch lecture-01  Changes to be committed: 	modified:   solution.go

--- solution.go
+++ solution.go
@@ -5,15 +5,20 @@ import (
 )
 
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
-type SidesType int
+type sidesType int
 
 // Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
 // it's like:
 // CalcSquare(10.0, SidesTriangle)
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
+const (
+	SidesTriangle sidesType = 3
+	SidesSquare   sidesType = 4
+	SidesCircle   sidesType = 0
+)
 
-func CalcSquare(sideLen float64, sidesNum SidesType) float64 {
+func CalcSquare(sideLen float64, sidesNum sidesType) float64 {
 	var square float64
 	switch sidesNum {
 	case 3:
