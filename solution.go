c3b33d3cc372c29ce850fd976bb86f5bef16277d
PoziTV <olkhovskij-dima@mail.ru>
2022-03-03 01:47:43 +0600

    01

--- solution.go
+++ solution.go
@@ -1,5 +1,7 @@
 package square
 
+import "math"
+
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
 
 // Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
@@ -8,5 +10,24 @@ package square
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+type SidesNum int
+
+const (
+	SidesCircle   SidesNum = 0
+	SidesTriangle SidesNum = 3
+	SidesSquare   SidesNum = 4
+)
+
+func CalcSquare(sideLen float64, sidesNum SidesNum) float64 {
+
+	switch sidesNum {
+	case SidesCircle:
+		return math.Pi * sideLen * sideLen
+	case SidesTriangle:
+		return sideLen * sideLen * math.Sqrt(3) / 4
+	case SidesSquare:
+		return sideLen * sideLen
+	default:
+		return 0
+	}
 }
