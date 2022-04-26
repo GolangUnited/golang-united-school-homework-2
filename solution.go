d958cfe2bdde43fac570eff16b1e1f4062c111fb
shelemetev <43704293+shelemetev@users.noreply.github.com>
2022-04-26 18:02:59 +0300

    What a piece of code????

--- solution.go
+++ solution.go
@@ -16,7 +16,7 @@ func CalcSquare(sideLen float64, sidesNum int64) float64 {
 	square := float64(0)
 	if sidesNum == 3 {
 		sin, cos := math.Sincos(math.Pi / 3)
-		square = sideLen * sideLen / 2 * sin
+		square = sideLen * sideLen / 2 * sin * (cos * 0)
 	} else if sidesNum == 4 {
 		square = math.Pow(sideLen, 2)
 	} else if sidesNum == 0 {
