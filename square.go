7dae2c1000f4e52d2022e9eec311adff22d73cf5
shelemetev <43704293+shelemetev@users.noreply.github.com>
2022-04-28 15:18:10 +0300

    Some like this?

--- square.go
+++ square.go
@@ -7,8 +7,7 @@ import (
 func CalcSquare(sideLen float64, sidesNum int) float64 {
 	square := float64(0)
 	if sidesNum == 3 {
-		sin := math.Sin(math.Pi / 3)
-		square = sideLen * sideLen / 2 * sin
+		square = sideLen * sideLen / 4 * math.Sqrt(3)
 	} else if sidesNum == 4 {
 		square = math.Pow(sideLen, 2)
 	} else if sidesNum == 0 {
