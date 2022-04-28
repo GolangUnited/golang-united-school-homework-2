8b82a30c85e4208c04cb37519850d1e59f556faf
shelemetev <43704293+shelemetev@users.noreply.github.com>
2022-04-28 15:32:01 +0300

    Мистер Инкредипл!(Суперсемейка, Бомб Вояж)

--- solution.go
+++ solution.go
@@ -5,12 +5,19 @@ import (
 )
 
 func CalcSquare(sideLen float64, sidesNum int) float64 {
+	SidesSquare := int(4)
+
+	SidesTriangle := int(3)
+
+	SidesCircle := int(0)
+
 	square := float64(0)
-	if sidesNum == 3 {
+
+	if sidesNum == SidesTriangle {
 		square = sideLen * sideLen / 4 * math.Sqrt(3)
-	} else if sidesNum == 4 {
+	} else if sidesNum == SidesSquare {
 		square = math.Pow(sideLen, 2)
-	} else if sidesNum == 0 {
+	} else if sidesNum == SidesCircle {
 		square = sideLen * math.Pi
 	} else {
 		print("Error. Not calculated because of incorrect data.")
