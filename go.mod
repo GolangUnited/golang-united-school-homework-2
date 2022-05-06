35809df3251c9400361939b76a0012c9610a84f3
Dm <dmkhost@gmail.com>
2022-05-06 10:36:40 +0200

    add go.mod file and modified slution.go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dmkovy/squareTask
+
+go 1.17
--- solution.go
+++ solution.go
@@ -1,12 +1,42 @@
-package square
+package main
+
+import (
+	"math"
+)
 
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
+type myCalcSquareInt int
 
 // Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
+const (
+	SidesCircle   myCalcSquareInt = 0
+	SidesTriangle myCalcSquareInt = 3
+	SidesSquare   myCalcSquareInt = 4
+)
+
 // it's like:
 // CalcSquare(10.0, SidesTriangle)
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+func CalcSquare(sideLen float64, sidesNum myCalcSquareInt) float64 {
+	var res float64
+	switch sidesNum {
+	case 0:
+		res = sideLen * sideLen * math.Pi // count sideLen as radius
+	case 3:
+		res = sideLen * sideLen / 2
+	case 4:
+		res = sideLen * sideLen
+	default:
+		res = 0
+	}
+	return res
 }
+
+//func main() {
+//	fmt.Println(CalcSquare(10.0, SidesTriangle))
+//	fmt.Println(CalcSquare(10.0, SidesSquare))
+//	fmt.Println(CalcSquare(10.0, SidesCircle))
+//	fmt.Println(CalcSquare(10.0, 2))
+//}
