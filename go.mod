1c716edd92356e5c574646befa68eeae476075f6
footooreal <107985446+footooreal@users.noreply.github.com>
2022-07-03 21:47:37 +0300

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/footooreal/golang-united-school-homework-2/upload/solution
+
+go 1.18
--- solution.go
+++ solution.go
@@ -8,5 +8,30 @@ package square
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+import (
+	"math"
+)
+
+const (
+	SidesTriangle CustomIntType = 3
+	SidesSquare   CustomIntType = 4
+	SidesCircle   CustomIntType = 0
+	pi                          = math.Pi
+)
+
+type CustomIntType int
+
+func CalcSquare(sideLen float64, sidesNum CustomIntType) float64 {
+	var result float64
+	switch sidesNum {
+	case 4:
+		result = sideLen * sideLen
+	case 3:
+		result = ((sideLen * sideLen) * math.Sqrt(3)) / 4
+	case 0:
+		result = pi * (sideLen * sideLen)
+	default:
+		result = 0.0
+	}
+	return result
 }
